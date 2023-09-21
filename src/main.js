4b231b29003355302b9141a072e9ab102dbae314
zhengxuqi027109@gtjas.com <zhengxuqi027109@gtjas.com>
2023-09-21 21:10:42 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
 import router from './router'
 
-const app = createApp(App)
+Vue.config.productionTip = false
 
-app.use(router)
-
-app.mount('#app')
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+Vue.use(Router)
+
+export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
-
-export default router
